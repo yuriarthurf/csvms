5097aa2f552bd67a8ea175bdb0be8ece95166244
Didone <didone@live.com>
2022-06-18 11:39:50 -0300

    Sphinx documentations

--- csvms/__init__.py
+++ csvms/__init__.py
@@ -1,5 +1,6 @@
 """ CSVMS Module """
 __version__ = '0.2.3'
+import re
 import logging
 from os import environ
 
@@ -7,3 +8,25 @@ logging.basicConfig()
 logger = logging.getLogger("CSVMS")
 logger.setLevel(getattr(logging, environ.get('LOG_LEVEL', 'INFO')))
 logger.info("version:%s", __version__)
+
+def pyproject(location:str) -> dict():
+    """ Parse toml file to dictionary
+    :return: Dictionary with toml structure
+    """
+    _props = dict()
+    _prop = str()
+    with open(location, encoding="utf-8") as _file:
+        _pyproject = _file.readlines()
+    for conf in _pyproject:
+        try:
+            _prop = next(re.finditer(r"^\[(.+)\]$", conf, re.IGNORECASE)).group(1)
+            _props[_prop]=dict()
+        except StopIteration:
+            try:
+                matches = next(re.finditer(r"""(.+) = (\"|\[)(.+)(\"|\])""", conf, re.IGNORECASE))
+                _props[_prop].update({matches.group(1).replace('\"',''):matches.group(3).replace('\"','')})
+            except StopIteration:
+                continue
+        except Exception as err:
+            raise err
+    return _props
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,30 +1,10 @@
 """Library Unittests"""
-import re
 import pytest
-from csvms import __version__
+from csvms import __version__, pyproject
 
 @pytest.fixture
 def toml() -> dict():
-    """ Parse toml file to dictionary
-    :return: Dictionary with toml structure
-    """
-    _props = dict()
-    _prop = str()
-    with open("pyproject.toml", encoding="utf-8") as _file:
-        _pyproject = _file.readlines()
-    for conf in _pyproject:
-        try:
-            _prop = next(re.finditer(r"^\[(.+)\]$", conf, re.IGNORECASE)).group(1)
-            _props[_prop]=dict()
-        except StopIteration:
-            try:
-                matches = next(re.finditer(r"""(.+) = (\"|\[)(.+)(\"|\])""", conf, re.IGNORECASE))
-                _props[_prop].update({matches.group(1).replace('\"',''):matches.group(3).replace('\"','')})
-            except StopIteration:
-                continue
-        except Exception as err:
-            raise err
-    yield _props    
+    yield pyproject("pyproject.toml")
 
 def test_version(toml):
     """ Check Version """
