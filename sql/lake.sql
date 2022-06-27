c1124762574a17fdab02ad9f19552e493dd42763
Didone <didone@live.com>
2022-06-27 16:32:43 -0300

    regex

--- sql/lake.sql
+++ sql/lake.sql
@@ -1,5 +1,5 @@
 --aws s3 sync bootcamp/examples/data/log s3://compass.uol.bootcamp/sql
-;
+--https://docs.aws.amazon.com/pt_br/athena/latest/ug/data-types.html
 CREATE EXTERNAL TABLE csvms.raw_lista_frutas(
     op string, 
     op_ts timestamp,
@@ -93,7 +93,7 @@ EXPLAIN ANALYZE VERBOSE  SELECT nm_fruta FROM raw_lista_frutas WHERE tp_fruta = 
 -- https://github.com/prestodb/presto/tree/master/presto-spi/src/main/java/com/facebook/presto/spi/plan
 
 --https://randomuser.me/api/?format=json&nat=br&results=5
---aws s3 sync bootcamp/sql/users s3://compass.uol.bootcamp/
+--aws s3 sync bootcamp/sql/users s3://compass.uol.bootcamp/users
 CREATE EXTERNAL TABLE csvms.randomuser (
   results array<
     struct<
@@ -192,19 +192,29 @@ SELECT users.results.gender
 CROSS JOIN UNNEST(randomuser.results) AS users (results)
 ;
 
---aws s3 sync bootcamp/sql/locations s3://compass.uol.bootcamp/         
+--aws s3 sync bootcamp/sql/locations s3://compass.uol.bootcamp/locations
 CREATE EXTERNAL TABLE csvms.raw_locations (
     results STRING)
 STORED AS TEXTFILE
 LOCATION 's3://compass.uol.bootcamp/locations';
 
 -- https://prestodb.io/docs/0.217/functions/json.html
-CREATE OR REPLACE VIEW csvms.locations AS
-  WITH prep AS (
-SELECT json_extract_scalar(results, '$') results
-  FROM csvms.raw_locations)
+--https://docs.aws.amazon.com/pt_br/athena/latest/ug/ctas-examples.html
+CREATE table csvms.locations
+WITH (
+      external_location = 's3://compass.uol.bootcamp/silver/locations/',
+      write_compression = 'SNAPPY',
+      format = 'PARQUET')
+    AS
 SELECT CAST(json_extract(prep.results,'$.city') AS VARCHAR) city
      , CAST(json_extract(prep.results,'$.state') AS VARCHAR) state
      , CAST(json_extract(prep.results,'$.country') AS VARCHAR) country
-  FROM prep
+     , CAST(element_at(
+        regexp_extract_all(CAST(json_extract(prep.results,'$.coordinates') AS VARCHAR) 
+      , '[0-9.-]+'),1) AS DOUBLE) longitude
+     , cast(element_at(
+        regexp_extract_all(CAST(json_extract(prep.results,'$.coordinates') AS VARCHAR) 
+      , '[0-9.-]+'),2) AS DOUBLE) latitude
+  FROM (SELECT json_extract_scalar(results, '$') results
+         FROM csvms.raw_locations) prep
 ;
