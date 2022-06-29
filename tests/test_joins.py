80f247a4c67af0e946c90b5a37facfe1ff57d8ae
Didone <didone@live.com>
2022-06-29 10:09:05 -0300

    ✅ cross join

--- tests/test_joins.py
+++ tests/test_joins.py
@@ -3,7 +3,7 @@
 from tests.test_operations import A, B
 
 def test_inner_join_projection():
-    """Test join projection operator"""
+    """Test inner join projection operator"""
     assert A.ᐅᐊ(B, where={'eq':['A.chave','B.chave']}).definition == {
         'name': 'mock.(A⋈B)',
         'columns': {
@@ -16,3 +16,10 @@ def test_inner_join_projection():
             'B.chave': 'integer','B.desc': 'text','B.valor': 'float',
             'A.chave': 'integer','A.desc': 'text','A.valor': 'float'}}
     assert [r for r in B.ᐅᐊ(A, where={'eq':['B.chave','A.chave']})] == [(1,'a',0.55,1,'a',0.55)]
+
+def test_cross_join():
+    """Test cross join operator"""
+    assert (A * B).σ({'eq':['A.chave','B.chave']}).columns == {
+        'A.chave': int, 'A.desc': str, 'A.valor': float,
+        'B.chave': int, 'B.desc': str, 'B.valor': float}
+    assert [r for r in (A * B).σ({'eq':['A.chave','B.chave']})] == [(1,'a',0.55,1,'a',0.55)]
