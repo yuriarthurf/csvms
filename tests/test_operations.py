92c7b5bf1fd2bbb4c16736566c5e10d89688e2fc
Didone <didone@live.com>
2022-06-29 09:53:54 -0300

    ✅Join

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -144,3 +144,26 @@ def test_rename_projection():
             'valor': 'float'
         }
     }
+
+def test_join_projection():
+    """Test join projection operator"""
+    assert A.ᐅᐊ(B, where={'eq':['A.chave','B.chave']}).definition == {
+        'name': 'mock.(A⋈B)',
+        'columns': {
+            'A.chave': 'integer',
+            'A.desc': 'text',
+            'A.valor': 'float',
+            'B.chave': 'integer',
+            'B.desc': 'text',
+            'B.valor': 'float'}}
+    assert [r for r in A.ᐅᐊ(B, where={'eq':['A.chave','B.chave']})] == [(1,'a',0.55,1,'a',0.55)]
+    assert B.ᐅᐊ(A, where={'eq':['B.chave','A.chave']}).definition == {
+        'name': 'mock.(B⋈A)',
+        'columns': {
+            'B.chave': 'integer',
+            'B.desc': 'text',
+            'B.valor': 'float',
+            'A.chave': 'integer',
+            'A.desc': 'text',
+            'A.valor': 'float'}}
+    assert [r for r in B.ᐅᐊ(A, where={'eq':['B.chave','A.chave']})] == [(1,'a',0.55,1,'a',0.55)]
