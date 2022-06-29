748db4295fe1d692ee0e309639a52b57456e333d
Didone <didone@live.com>
2022-06-29 09:08:18 -0300

    ✅Union

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -20,3 +20,17 @@ B = Table(
     data=[
         (1,"a",0.55),
         (2,"b",1.05)])
+
+def test_union():
+    """Test union operator"""
+    assert (A + B).definition == {
+        "name": "mock.(A∪B)",
+        "columns": {
+            "chave": "integer",
+            "desc": "text",
+            "valor": "float"}}
+    assert [r for r in (A + B)] == [
+        (1,"a",0.55),
+        (3,"c",9.99),
+        (2,"b",1.05)
+    ]
