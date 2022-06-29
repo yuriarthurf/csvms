80b746a5d3e649ca29a2fc8b520cddaf63d33026
Didone <didone@live.com>
2022-06-29 09:28:03 -0300

    ✅ selection

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -9,7 +9,7 @@ A = Table(
         "valor":float},
     data=[
         (1,"a",0.55),
-        (3,"c",9.99)])
+        (3,"c",None)])
 
 B = Table(
     name="pytest.B",
@@ -31,7 +31,7 @@ def test_union():
             "valor": "float"}}
     assert [r for r in (A + B)] == [
         (1,"a",0.55),
-        (3,"c",9.99),
+        (3,"c",None),
         (2,"b",1.05)
     ]
 
@@ -53,7 +53,7 @@ def test_diff():
             "chave": "integer",
             "desc": "text",
             "valor": "float"}}
-    assert [r for r in (A - B)] == [(3,"c",9.99)]
+    assert [r for r in (A - B)] == [(3,"c",None)]
 
     assert (B - A).definition == {
         "name": "mock.(B−A)",
@@ -77,6 +77,34 @@ def test_product():
     assert [r for r in (A * B)] == [
         (1, 'a', 0.55, 1, 'a', 0.55),
         (1, 'a', 0.55, 2, 'b', 1.05),
-        (3, 'c', 9.99, 1, 'a', 0.55),
-        (3, 'c', 9.99, 2, 'b', 1.05)
+        (3, 'c', None, 1, 'a', 0.55),
+        (3, 'c', None, 2, 'b', 1.05)
     ]
+
+def test_selection():
+    """Test selection operator"""
+    # A.σ({'and':[{"eq":['Manager','George']},{"lt":['DeptId',2]}]})
+    assert A.σ({"eq":['chave',1]}).definition == {
+        'name': 'mock.(Aσ)',
+        'columns': {
+            'chave': 'integer',
+            'desc': 'text',
+            'valor': 'float'}}
+    assert [r for r in A.σ({'and':[
+            {"eq":['chave',1]},
+            {"exists":'valor'}]})] == [(1,"a",0.55)]
+    assert [r for r in A.σ({'or':[
+            {"eq":['chave',1]},
+            {"missing":'valor'}]})] == [(1,"a",0.55),(3,"c",None)]
+    assert [r for r in A.σ({"lt":['chave',3]})] == [(1,"a",0.55)]
+    assert [r for r in A.σ({"gt":['chave',1]})] == [(3,"c",None)]
+    assert [r for r in A.σ({"eq":['chave',1]})] == [(1,"a",0.55)]
+    assert [r for r in A.σ({"lte":['chave',3]})] == [(1,"a",0.55),(3,"c",None)]
+    assert [r for r in A.σ({"gte":['chave',1]})] == [(1,"a",0.55),(3,"c",None)]
+    assert [r for r in A.σ({"neq":['chave',1]})] == [(3,"c",None)]
+    assert [r for r in A.σ({"is":['valor',None]})] == [(3,"c",None)]
+    assert [r for r in A.σ({"missing":'valor'})] == [(3,"c",None)]
+    assert [r for r in A.σ({"exists":'valor'})] == [(1,"a",0.55)]
+    assert [r for r in A.σ({"in":['desc','ab']})] == [(1,"a",0.55)]
+    assert [r for r in A.σ({"nin":['desc','ab']})] == [(3,"c",None)]
+
