583745ad24d329b1296ee0e18c770f787cf15ca1
Didone <didone@live.com>
2022-06-29 09:58:05 -0300

    👕 remove linter messages

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,5 @@
 """Test Operators"""
 from csvms.table import Table
-from tomlkit import value
 
 A = Table(
     name="pytest.A",
@@ -69,12 +68,8 @@ def test_product():
     assert (A * B).definition == {
         "name": "mock.(A×B)",
         "columns": {
-            "A.chave": "integer",
-            "A.desc": "text",
-            "A.valor": "float",
-            "B.chave": "integer",
-            "B.desc": "text",
-            "B.valor": "float"}}
+            "A.chave": "integer","A.desc": "text","A.valor": "float",
+            "B.chave": "integer","B.desc": "text","B.valor": "float"}}
     assert [r for r in (A * B)] == [
         (1, 'a', 0.55, 1, 'a', 0.55),
         (1, 'a', 0.55, 2, 'b', 1.05),
@@ -150,20 +145,12 @@ def test_join_projection():
     assert A.ᐅᐊ(B, where={'eq':['A.chave','B.chave']}).definition == {
         'name': 'mock.(A⋈B)',
         'columns': {
-            'A.chave': 'integer',
-            'A.desc': 'text',
-            'A.valor': 'float',
-            'B.chave': 'integer',
-            'B.desc': 'text',
-            'B.valor': 'float'}}
+            'A.chave': 'integer','A.desc': 'text','A.valor': 'float',
+            'B.chave': 'integer','B.desc': 'text','B.valor': 'float'}}
     assert [r for r in A.ᐅᐊ(B, where={'eq':['A.chave','B.chave']})] == [(1,'a',0.55,1,'a',0.55)]
     assert B.ᐅᐊ(A, where={'eq':['B.chave','A.chave']}).definition == {
         'name': 'mock.(B⋈A)',
         'columns': {
-            'B.chave': 'integer',
-            'B.desc': 'text',
-            'B.valor': 'float',
-            'A.chave': 'integer',
-            'A.desc': 'text',
-            'A.valor': 'float'}}
+            'B.chave': 'integer','B.desc': 'text','B.valor': 'float',
+            'A.chave': 'integer','A.desc': 'text','A.valor': 'float'}}
     assert [r for r in B.ᐅᐊ(A, where={'eq':['B.chave','A.chave']})] == [(1,'a',0.55,1,'a',0.55)]
