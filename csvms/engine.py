2810bc6f049ee427757237341554a54b73cc1970
Yuri Arthur Ferreira Santana <yuri.santana@pampa.compasso>
2022-07-12 10:42:15 -0300

    #7 Criação de tabelas

--- csvms/engine.py
+++ csvms/engine.py
@@ -1,36 +1,22 @@
 from csvms.table import Table
 from mo_sql_parsing import parse
 
-#Usando o parse do mo sql parsing para utilizar o dict de output na criação da tabela
-print(parse("CREATE TABLE lista_frutas (nm_fruta TEXT ,tp_fruta TEXT )"))
+def interpretador_sql(query):
 
+        #Usando o parse do mo sql parsing para utilizar o dict de output na criação da tabela
+        traduzido = parse(query)
 
-#Criação da tabela lista_frutas
-Table(name="lista_frutas", 
-        columns={
-                'nm_fruta':str, 
-                'tp_fruta': str}).save()
+        #Criação da tabela lista_frutas
+        try:
+                Table(name=traduzido['create table']['name'], 
+                        columns={
+                                traduzido['create table']['columns'][0]['name']:str, 
+                                traduzido['create table']['columns'][1]['name']:str}).save()
+                print('Tabela criada com sucesso')
+        except:
+                return print('Não foi possível criar a tabela')
+        
+        table = Table('lista_frutas')
+        return print(table) 
 
-#Chamando a tabela lista_frutas
-table = Table('lista_frutas')
-
-#Adicionando dados à tabela
-table.append('banana', 'doce')
-table.append('limão', 'amargo')
-table.append('bergamota', 'azedo')
-table.append('maçã', 'doce')
-print(table)
-table.save()
-
-#Adicionando dados de outra forma 
-Table(name="lista_frutas2", 
-        columns={
-                'nm_fruta':str, 
-                'tp_fruta': str},
-        data=
-                [
-                ('banana', 'doce'),
-                ('limão', 'amargo'),
-                ('bergamota', 'azedo'),
-                ('maçã', 'doce')
-                ]).save()
+interpretador_sql("CREATE TABLE lista_frutas (nm_fruta TEXT ,tp_fruta TEXT )")
