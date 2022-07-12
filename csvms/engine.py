338f1b74c3d486965b7d1ddfea3aa850e2a5ecba
Yuri Arthur Ferreira Santana <yuri.santana@pampa.compasso>
2022-07-12 17:19:27 -0300

    #8 Inserção de dados v3

--- csvms/engine.py
+++ csvms/engine.py
@@ -3,26 +3,46 @@ from mo_sql_parsing import parse
 import re
 
 def interpretador_sql(query):
-        query = query.split(";")
-        new_query = []
-        values = []
-        for i in query:
-                new_query.append(i.replace('\n',''))
+        if 'CREATE' in query:
+                traduzido = parse(query)
+                try:
+                        Table(name=traduzido['create table']['name'], 
+                                columns={
+                                        traduzido['create table']['columns'][0]['name']:str, 
+                                        traduzido['create table']['columns'][1]['name']:str}).save()
+                        print('Tabela criada com sucesso')
+                except:
+                        return print('Não foi possível criar a tabela')
 
-        for j in new_query:
-                values.append(new_query[new_query.index(j)].replace("INSERT INTO lista_frutas VALUES (", ''))
+                table = Table('lista_frutas')
+                return print(table) 
         
-        values.pop(-1)
+        if 'INSERT' in query:
+                query = query.split(";")
+                new_query = []
+                values = []
+                for i in query:
+                        new_query.append(i.replace('\n',''))
 
-        table = Table('lista_frutas')
+                for j in new_query:
+                        values.append(new_query[new_query.index(j)].replace("INSERT INTO lista_frutas VALUES (", ''))
+                
+                values.pop(-1)
 
-        for k in range(len(values)):
-                values[k] = values[k].replace(')', '')
-                table.append(values[k].split(',')[0].replace("'", ""), values[k].split(',')[1].replace("'", ""))
+                table = Table('lista_frutas')
 
-        table.save()
+                for k in range(len(values)):
+                        values[k] = values[k].replace(')', '')
+                        table.append(values[k].split(',')[0].replace("'", ""), values[k].split(',')[1].replace("'", ""))
 
-        return print(table) 
+                table.save()
+
+                return print(table) 
+
+interpretador_sql("""CREATE TABLE lista_frutas (
+    nm_fruta TEXT ,
+    tp_fruta TEXT 
+)""")
 
 interpretador_sql("""INSERT INTO lista_frutas VALUES ('banana','doce');
 INSERT INTO lista_frutas VALUES ('limão','amargo');
