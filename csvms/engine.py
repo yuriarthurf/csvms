94679216afd90f5f589960adacfeb4e2a92e11a6
Yuri Arthur Ferreira Santana <yuri.santana@pampa.compasso>
2022-07-11 22:00:25 -0300

    #8 Incluindo dados

--- csvms/engine.py
+++ csvms/engine.py
@@ -6,8 +6,31 @@ print(parse("CREATE TABLE lista_frutas (nm_fruta TEXT ,tp_fruta TEXT )"))
 
 
 #Criação da tabela lista_frutas
-table = Table(name="lista_frutas", 
-                columns={
-                        'nm_fruta':str, 
-                        'tp_fruta': str}).save()
+Table(name="lista_frutas", 
+        columns={
+                'nm_fruta':str, 
+                'tp_fruta': str}).save()
+
+#Chamando a tabela lista_frutas
+table = Table('lista_frutas')
+
+#Adicionando dados à tabela
+table.append('banana', 'doce')
+table.append('limão', 'amargo')
+table.append('bergamota', 'azedo')
+table.append('maçã', 'doce')
 print(table)
+table.save()
+
+#Adicionando dados de outra forma
+Table(name="lista_frutas2", 
+        columns={
+                'nm_fruta':str, 
+                'tp_fruta': str},
+        data=
+                [
+                ('banana', 'doce'),
+                ('limão', 'amargo'),
+                ('bergamota', 'azedo'),
+                ('maçã', 'doce')
+                ]).save()
